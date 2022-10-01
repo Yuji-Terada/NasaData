6c59cc73d296edeffa57fe52c495f3ce3520c99d
Yuji070902 <93475805+Yuji-Terada@users.noreply.github.com>
2022-10-02 03:30:29 +0900

    Update Earth.js

--- Earth.js
+++ Earth.js
@@ -1,8 +1,16 @@
 var form = document.forms.images;
-var N = form.image.value;
-    
+
+let N = 0
+
+const renderEarth = (e) => {
+    N = e.target.value;
     Globe()
-      .globeImageUrl(`./${N}.jpg`)
-      .pointAltitude('size')
-      .pointColor('color')
-    (document.getElementById('globeViz'))
+        .globeImageUrl(`./${N}.jpg`)
+        .pointAltitude('size')
+        .pointColor('color')
+    (document.getElementById('globeViz');
+}
+
+form.addEventListener('change', renderEarth)
+
+renderEarth({target: {value: 0}})
