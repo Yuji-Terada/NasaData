fafcbc09c9f597efb2b5f469e57a1ed05037fd39
Yuji070902 <93475805+Yuji-Terada@users.noreply.github.com>
2022-10-02 02:16:51 +0900

    Add files via upload

---
+++ Earth.js
@@ -0,0 +1,24 @@
+const N = 0
+
+var imgs = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg"]
+ 
+items.push(5,6,7,8);
+ 
+console.log(items);
+
+Globe()
+    .globeImageUrl(imgs[N])
+    .pointsData(gData)
+    .pointAltitude('size')
+    .pointColor('color')
+(document.getElementById('globeViz'))
